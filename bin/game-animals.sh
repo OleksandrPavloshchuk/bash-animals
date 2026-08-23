e816d4507f0f6620132f0f106d2dbf3b94a8560b
Oleksandr Pavloshchuk <oleksandr.pavloshchuk@openiam.com>
2026-08-23 15:53:52 +0300

    read animal from base

--- bin/game-animals.sh
+++ bin/game-animals.sh
@@ -3,13 +3,16 @@
 # debug
 #set -x
 
+GREEN=$'\033[32m'
+RESET=$'\033[0m'
+
 greeting() {
 	echo "Welcome in animals game!"
 }
 
 read_yes_or_no() {
 	while true; do
-		read -r -p "(y/n) " YES_OR_NO
+		read -r -p "$1 (y/n) " YES_OR_NO
 		if [[ "$YES_OR_NO" == "y" || "$YES_OR_NO" == "n" ]]; then
 			break
 		fi
@@ -17,11 +20,31 @@ read_yes_or_no() {
 	echo "yes/no = ${YES_OR_NO}"
 }
 
+read_animal_from_base() {
+	IFS=',' read -r NAME QUESTION YES_ID NO_ID < "../data/$ANIMAL_ID"	
+}
+
+ask_animal() {
+	read_yes_or_no "Is is a ${GREEN}${NAME}${RESET}?"
+}
+
 #---
 greeting
 
-read_yes_or_no
+ANIMAL_ID="root"
 
-echo "out = ${YES_OR_NO}"
+# main loop
+while true; do
+	read_animal_from_base
+	# TODO remove trace
+	echo "name=$NAME, question=$QUESTION, yes_id=$YES_ID, no_id=$NO_ID"
+
+	ask_animal
+	
+	
+	# TODO
+	break	
+	
+done # main loop
 
 
